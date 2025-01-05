a5b137cbc9a95545a41a1280878e1c3639bbd40f
Allen <pan.zhan00@gmail.com>
2025-01-05 21:00:24 +1100

    fix: fix image not displaying issue

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
-export default {
+module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-        search: ''
       },
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
-        search: ''
       }
     ]
   }
