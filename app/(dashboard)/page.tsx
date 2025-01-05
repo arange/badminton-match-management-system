ef374fe17505feaa54748b39f75e1897a12a3317
Allen <pan.zhan00@gmail.com>
2025-01-05 17:01:51 +1100

    fix: fix bug

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { MatchesTable } from './matches-table';
 import { getMatches } from '@/lib/db';
+import { MatchesTable } from './matches-table';
 
 export default async function MatchesPage(
   props: {
