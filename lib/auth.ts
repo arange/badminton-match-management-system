d48ecc904be8b95ebb67c6df2b83834772cf487e
Allen <pan.zhan00@gmail.com>
2025-01-05 20:34:00 +1100

    feat: Config NextAuth with Github

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub]
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 });
